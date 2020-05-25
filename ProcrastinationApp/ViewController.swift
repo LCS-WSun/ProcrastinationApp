4c74fee3e40c9d3ebac3fbf0faf5ffd5580f04e2
Wesley Sun <wsun@lcs.on.ca>
2020-05-25 11:14:03 -0400

    Initial Commit

---
+++ ProcrastinationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProcrastinationApp
+//
+//  Created by Sun, Wesley on 2020-05-25.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
