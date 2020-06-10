a05dd906621ad4537f72120d98e2a7a811d21c7f
Wesley Sun <wsun@lcs.on.ca>
2020-06-10 06:56:44 -0400

    Overall all I have one is created the user interface. Will tackle this during the Summer

---
+++ ProcrastinationApp/WorkTimerStart.swift
@@ -0,0 +1,31 @@
+//
+//  WorkTimerStart.swift
+//  ProcrastinationApp
+//
+//  Created by Sun, Wesley on 2020-06-07.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WorkTimerStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//Home Screen Code
+//no need for code
+    
+    
+//WORK? Screen Code
+    
+    
+    
+    
+    
+
+
+}
